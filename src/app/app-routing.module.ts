4fc799b207fec0adce795c6075b4f6195f20b3c4
Ray <ray_punkeki@hotmail.com>
2023-09-26 11:48:50 -0500

    feat: forgot password style added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,13 @@ import { LoginComponent } from './components/login/login.component';
 import { EmployerPostComponent } from './components/employer-post/employer-post.component';
 import { RegisterComponent } from './components/register/register.component';
 import { JobPostComponent } from './components/job-post/job-post.component';
+import { AuthComponent } from './components/auth/auth.component';
+
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full'},
   { path: 'post/:id', component: EmployerPostComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'auth', component: AuthComponent},
   { path: 'login', component: LoginComponent },
   { path: 'job/post', component: JobPostComponent }
 
